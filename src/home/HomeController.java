e98f5a29ec9dcd601e775f56247ffb160b7f4ebd
parksunyoung <iss002@naver.com>
2021-09-23 19:20:01 +0900

    psy:: BookDTO 수정.

--- src/home/HomeController.java
+++ src/home/HomeController.java
@@ -7,17 +7,18 @@ import common.Common;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 
-
-public class HomeController implements Initializable{
+public class HomeController implements Initializable {
 
 	Parent root = null;
+
 	public void setRoot(Parent p) {
 		this.root = p;
+
 	}
-	
-	
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		Common.MyConnection();
 	}
+
 }
